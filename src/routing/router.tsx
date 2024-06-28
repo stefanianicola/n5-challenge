5b104d860c14a912458bb08caf32df59ec37f224
stefanianicola <stefanianicola@gmail.com>
2024-06-27 21:31:28 -0300

    fix routes

--- src/routing/router.tsx
+++ src/routing/router.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Cart from '../components/Cart';
 import ListItems from '../components/ListItems';
 import AddNewItem from '../components/AddNewItem/AddNewItem';
@@ -7,8 +7,8 @@ import TestLogic from '../components/TestLogic';
 export const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/" element={<ListItems />} />
-      <Route path="/*" element={<ListItems />} />
+      <Route path="/" element={<Navigate to="/home" />} />
+      <Route path="/home" element={<ListItems />} />
       <Route path="/cart" element={<Cart />} />
       <Route path="/add-item" element={<AddNewItem />} />
       <Route path="/logica" element={<TestLogic />} />
